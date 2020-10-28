08aa529212bd5e57bbea41d2deffab213a9d5ef3
NORDIC\pkarim <paria.karim@gmail.com>
2020-10-28 13:52:36 +0100

    feat(CandyController): add addCandy method

--- src/main/java/com/experis/candy_manufactory/controllers/CandyController.java
+++ src/main/java/com/experis/candy_manufactory/controllers/CandyController.java
@@ -1,7 +1,12 @@
 package com.experis.candy_manufactory.controllers;
 
 import com.experis.candy_manufactory.Repositories.CandyRepository;
+import com.experis.candy_manufactory.models.Candy;
+import com.experis.candy_manufactory.utils.CommonResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +19,15 @@ public class CandyController {
     @Autowired
     private CandyRepository candyRepository;
 
-   
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    private ResponseEntity<CommonResponse> addCandy(@RequestBody Candy candyToAdd){
+        CommonResponse commonResponse = new CommonResponse();
+
+        candyToAdd = candyRepository.save(candyToAdd);
+
+        commonResponse.data = candyToAdd;
+        commonResponse.message = "The candy: " + candyToAdd + " has been added!";
+
+        return new ResponseEntity<>(commonResponse, HttpStatus.CREATED);
+    }
 }
