95672fd26610c69d0201584a321a52787e8363b8
NORDIC\pkarim <paria.karim@gmail.com>
2020-10-28 11:42:39 +0100

    :tada: first commit of project skeleton

---
+++ src/main/java/com/experis/candy_manufactory/CandyManufactoryApplication.java
@@ -0,0 +1,13 @@
+package com.experis.candy_manufactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandyManufactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandyManufactoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/experis/candy_manufactory/CandyManufactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experis.candy_manufactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandyManufactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
