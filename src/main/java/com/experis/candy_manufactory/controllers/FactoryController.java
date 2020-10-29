4712bee4713ec579c94ed93b03da07be11be5b57
NORDIC\pkarim <paria.karim@gmail.com>
2020-10-29 12:01:41 +0100

    feat(FactoryController)added addFactory method

--- src/main/java/com/experis/candy_manufactory/controllers/FactoryController.java
+++ src/main/java/com/experis/candy_manufactory/controllers/FactoryController.java
@@ -1,14 +1,13 @@
 package com.experis.candy_manufactory.controllers;
 
 import com.experis.candy_manufactory.Repositories.FactoryRepository;
+import com.experis.candy_manufactory.models.Factory;
 import com.experis.candy_manufactory.utils.CommonResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.function.EntityResponse;
 
 @RequestMapping(value = "/api/v1/factory")
 
@@ -18,20 +17,33 @@ public class FactoryController {
     @Autowired
     FactoryRepository factoryRepository;
 
-    @RequestMapping(method = RequestMethod.GET)
-    private ResponseEntity<CommonResponse> getFactoryById(@PathVariable("id") Long id){
+    @RequestMapping(method = RequestMethod.POST)
+    private ResponseEntity<CommonResponse> addFactory(@RequestBody Factory factoryToAdd) {
+        CommonResponse commonResponse = new CommonResponse();
+
+        factoryToAdd = factoryRepository.save(factoryToAdd);
+
+        commonResponse.data = factoryToAdd;
+        commonResponse.message = "Factory with name: " + factoryToAdd.getFactoryName() +
+                " and id: " + factoryToAdd.getId() + " has been added.";
+
+        return new ResponseEntity<>(commonResponse, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    private ResponseEntity<CommonResponse> getFactoryById( @PathVariable("id") Long id){
         CommonResponse commonResponse = new CommonResponse();
         HttpStatus httpStatus;
 
         if(factoryRepository.existsById(id)){
             commonResponse.data = factoryRepository.findById(id);
             commonResponse.message = "Factory with id: " + id + " was found.";
-            httpStatus = HttpStatus.OK
+            httpStatus = HttpStatus.OK;
         } else {
             commonResponse.message = "Factory with id: " + id + " was not found.";
             httpStatus = HttpStatus.NOT_FOUND;
         }
-        return (commonResponse, httpStatus);
+        return new ResponseEntity<>(commonResponse, httpStatus);
     }
 
 }
