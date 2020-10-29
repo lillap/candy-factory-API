ea9facf2190812c675fe87733ff82b9800c03c85
NORDIC\pkarim <paria.karim@gmail.com>
2020-10-29 11:29:28 +0100

    feat(Factory): Added getters and setters

--- src/main/java/com/experis/candy_manufactory/models/Factory.java
+++ src/main/java/com/experis/candy_manufactory/models/Factory.java
@@ -14,4 +14,28 @@ public class Factory {
 
     @Column
     private int sizeArea;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFactoryName() {
+        return factoryName;
+    }
+
+    public void setFactoryName(String factoryName) {
+        this.factoryName = factoryName;
+    }
+
+    public int getSizeArea() {
+        return sizeArea;
+    }
+
+    public void setSizeArea(int sizeArea) {
+        this.sizeArea = sizeArea;
+    }
 }
