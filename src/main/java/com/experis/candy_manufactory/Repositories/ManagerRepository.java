d033a9594bd937d60f39e757195d334964bbc123
NORDIC\pkarim <paria.karim@gmail.com>
2020-10-29 13:30:22 +0100

    feat(ManagerRepository): added ModelRepository

---
+++ src/main/java/com/experis/candy_manufactory/Repositories/ManagerRepository.java
@@ -0,0 +1,9 @@
+package com.experis.candy_manufactory.Repositories;
+
+import com.experis.candy_manufactory.models.Manager;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ManagerRepository extends JpaRepository<Manager, Long> {
+}
